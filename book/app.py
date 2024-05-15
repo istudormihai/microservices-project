4a6a96631fb2460f83d41bc6616f81a17b8a3131
Istudor Mihai <istudor9@gmail.com>
2024-05-15 20:43:11 +0300

    added book microservice and book images

---
+++ book/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from routes import book_blueprint
+from models import db, init_app
+from flask_migrate import Migrate
+
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'NxU7k9N3oz_8hr2TMn_2sg'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.register_blueprint(book_blueprint)
+
+# Define the database URI
+database_file = os.path.join(os.getcwd(), 'database', 'book.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_file}'
+
+# Ensure the database directory exists
+os.makedirs(os.path.dirname(database_file), exist_ok=True)
+
+init_app(app)
+
+migrate = Migrate(app, db)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
---
+++ book/routes.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, request, jsonify
+from models import Book, db
+
+book_blueprint = Blueprint('book_api_routes', __name__, url_prefix='/api/book')
+
+@book_blueprint.route('/all', methods=['GET'])
+def get_all_books():
+    all_books = Book.query.all()
+    result = [book.serialize() for book in all_books]
+    response = { 'result': result }
+    return jsonify(response)
+
+@book_blueprint.route('/create', methods=['POST'])
+def create_book():
+    try:
+        book = Book()
+        book.name = request.form['name']
+        book.slug = request.form['slug']
+        book.price = request.form['price']
+        book.image = request.form['image']
+
+        db.session.add(book)
+        db.session.commit()
+
+        response = { 'message': 'Book created successfully', 'result': book.serialize() }
+    except Exception as e:
+        print(str(e))
+        response = { 'message': 'Error creating book' }
+
+    return jsonify(response)
+
+@book_blueprint.route('/<slug>', methods=['GET'])
+def book_details(slug):
+    book = Book.query.filter_by(slug=slug).first()
+    if book:
+        response = { 'result': book.serialize() }
+    else:
+        response = { 'message': 'Book not found' }
+
+    return jsonify(response)
